4e70418f04b3cd4686995e90db7905d2266f5435
Cynshiii <cynthiaemleigh@gmail.com>
2023-05-04 20:50:00 -0500

    Add exceptions

---
+++ src/main/java/net/projectmythos/argos/framework/exceptions/preconfigured/MissingArgumentException.java
@@ -0,0 +1,9 @@
+package net.projectmythos.argos.framework.exceptions.preconfigured;
+
+public class MissingArgumentException extends PreConfiguredException {
+
+	public MissingArgumentException() {
+		super("Missing argument");
+	}
+
+}
