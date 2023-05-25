033b5f3af27c1d81d551d141a7647cf37607a1f3
Cynshiii <cynthiaemleigh@gmail.com>
2023-05-25 00:14:38 -0500

    annotations work

--- src/main/java/net/projectmythos/argos/framework/commands/models/annotations/Async.java
+++ src/main/java/net/projectmythos/argos/framework/commands/models/annotations/Async.java
@@ -1,15 +1,11 @@
-package net.projectmythos.argos.framework.annotations;
-
-import net.projectmythos.argos.models.afk.AFKUser;
+package net.projectmythos.argos.framework.commands.models.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ObjectClass {
-    Class<AFKUser> value();
-
+public @interface Async {
 }
