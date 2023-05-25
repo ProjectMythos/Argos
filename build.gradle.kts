1a53cecd63a2f7033fdad0f417c2e1908e793fca
Cynshiii <cynthiaemleigh@gmail.com>
2023-05-25 00:15:58 -0500

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("dev.morphia.morphia:core:1.6.2-SNAPSHOT")
     implementation("dev.dbassett:skullcreator:3.0.1")
     implementation("com.squareup.okhttp3:okhttp:3.14.6")
+    implementation("org.objenesis:objenesis:3.2")
     compileOnly("gg.projecteden.parchment:parchment-api:1.19.4-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-core:7.0.6-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.6-SNAPSHOT")
@@ -40,6 +41,7 @@ dependencies {
         exclude("*", "*")
     }
     compileOnly("de.tr7zw:item-nbt-api-plugin:2.10.0")
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0")
     compileOnly("com.arcaniax:HeadDatabase-API:1.3.1")
     compileOnly(files("libs/GlowAPI_v1.5.7-SNAPSHOT.jar"))
 }
